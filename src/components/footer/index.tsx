f3439981bee0b082717702c804afd84b4f00b4a7
Shamayel Torabi <sh-torabi@hrec.co.ir>
2025-03-10 11:18:49 +0330

    [ADD] update pages

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <h1>Footer</h1>
+    </footer>
+  )
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex items-center h-16 bg-gray-600 p-4 text-white">
+            <h1>Header</h1>
+        </header>
+    )
+}
